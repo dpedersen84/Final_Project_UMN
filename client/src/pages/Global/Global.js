68abc44a82cea4425a92a3851cd7fddc8bcee550
Dan <dpedersen84@gmail.com>
2018-08-02 12:59:39 -0500

    adjustment to css and grid for imagecard component

--- client/src/pages/Global/Global.js
+++ client/src/pages/Global/Global.js
@@ -156,7 +156,7 @@ class Global extends Component {
                         value={this.state.comment}
                         onChange={this.handleInputChange}
                     /> */
-                ))};
+                ))}
             </div> 
         );
     };
